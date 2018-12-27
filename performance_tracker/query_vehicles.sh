de138b481daa07ece8a933c1b2933140aa18abe4
Cam Sexton <cameron.t.sexton@gmail.com>
2018-12-26 19:15:01 -0800

    resolve env path correctly

--- performance_tracker/query_vehicles.sh
+++ performance_tracker/query_vehicles.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-source env/bin/activate
-python ./download_raw_data.py lametro-rail 801
-python ./download_raw_data.py lametro-rail 802
-python ./download_raw_data.py lametro-rail 803
-python ./download_raw_data.py lametro-rail 804
-python ./download_raw_data.py lametro-rail 805
-python ./download_raw_data.py lametro-rail 806
+source ../env/bin/activate
+python ./query_vehicles.py lametro-rail 801
+python ./query_vehicles.py lametro-rail 802
+python ./query_vehicles.py lametro-rail 803
+python ./query_vehicles.py lametro-rail 804
+python ./query_vehicles.py lametro-rail 805
+python ./query_vehicles.py lametro-rail 806
 deactivate
