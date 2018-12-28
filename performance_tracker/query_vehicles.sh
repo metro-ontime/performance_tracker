cf7f957bf33d124acb4ec277490a755324018de8
Cam Sexton <cameron.t.sexton@gmail.com>
2018-12-27 22:44:44 -0800

    fix crontab

--- performance_tracker/query_vehicles.sh
+++ performance_tracker/query_vehicles.sh
@@ -2,4 +2,5 @@
 
 # Please provide root directory of performance_tracker repo as cmd line arg
 
+[ -z "$var" ] && export USER=ubuntu
 docker run -u $(id -u ${USER}):$(id -u ${USER}) -it --rm -v $1:/src --name testrun metro python performance_tracker/query_vehicles.py
