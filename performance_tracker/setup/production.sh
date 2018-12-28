abdbafe1b03d2c015d235ebbc8aa50b69f269d26
Cam Sexton <cameron.t.sexton@gmail.com>
2018-12-27 16:04:07 -0800

    add production server setup script

---
+++ performance_tracker/setup/production.sh
@@ -0,0 +1,6 @@
+mkdir ~/performance_tracker-worktree
+mkdir ~/performance_tracker-gitdir
+git init --bare ~/performance_tracker-gitdir
+cp post-receive.sample ~/performance_tracker-gitdir/hooks/post-receive
+chmod +x ~/performance_tracker-gitdir/hooks/post-receive
+echo "Bare git repo setup to receive pushes in ~/performance_tracker-gitdir and deploy"
