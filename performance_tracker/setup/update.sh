fcc55e64e3b023a4c2642a9f0ee139de15d57636
Cam Sexton <cameron.t.sexton@gmail.com>
2018-12-27 23:26:42 -0800

    update path for sample production crontab

--- performance_tracker/setup/update.sh
+++ performance_tracker/setup/update.sh
@@ -3,6 +3,6 @@
 # Provide path of repo root directory as cmd line arg
 
 docker build -t metro $1
-crontab $1/performance_tracker/setup/sample_crontab
+crontab $1/performance_tracker/setup/production_crontab
 echo "New crontab config:"
 crontab -l
