c25cf0c762fc3575b6556a804d537ce52a5214ef
Cam Sexton <cameron.t.sexton@gmail.com>
2019-01-26 15:05:42 -0800

    fix echo output

--- performance_tracker/get-process-upload_schedule.sh
+++ performance_tracker/get-process-upload_schedule.sh
@@ -11,8 +11,8 @@ bash ./get-process_schedule.sh $1
 
 docker run -u $(id -u $1):$(id -g $1) --rm -v $(pwd):/src metro python upload_schedule.py
 if [ $? -eq 0 ]; then
-  echo "Successfully uploaded schedule to S3:" $(date) >> $(pwd)/logs/uploadLog
+  echo "Successfully uploaded schedule to S3:" $(date)
 else
-  echo "Failed S3 upload:" $(date) >> $(pwd)/logs/uploadLog
+  echo "Failed S3 upload:" $(date)
   exit 1
 fi
