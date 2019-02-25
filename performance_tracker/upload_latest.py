086113efdb8a7fd6f721ce65b97dda94e3d5d18b
Cam Sexton <cameron.t.sexton@gmail.com>
2019-02-25 03:19:47 -0800

    fix broken script

--- performance_tracker/upload_latest.py
+++ performance_tracker/upload_latest.py
@@ -8,7 +8,7 @@ agency = "lametro-rail"
 
 for line in range(801, 807):
     schedule_base_path = f"data/schedule/{line}_{agency}"
-    schedule_meta = get_appropriate_timetable(datetime, schedule_base_path)
+    schedule_meta = get_appropriate_timetable(now, schedule_base_path)
     date = schedule_meta["date"]
     summary_path = f"data/summaries/{line}_{agency}/{date}.json"
     positions_path = f"data/vehicle_tracking/processed/{line}_{agency}/{date}.csv"
