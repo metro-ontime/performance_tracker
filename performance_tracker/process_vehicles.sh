fb5d86620cba9c869179509eaef703b9cc301c6d
Cam Sexton <cameron.t.sexton@gmail.com>
2019-03-01 01:36:59 -0800

    increase CPU allowance for processing and estimating scripts

--- performance_tracker/process_vehicles.sh
+++ performance_tracker/process_vehicles.sh
@@ -9,7 +9,7 @@ fi
 
 # We limit cpu usage to 50% as this process is resource intensive 
 # and we don't mind if it takes a few minutes extra to run
-docker run --cpus=".5" -u $(id -u $1):$(id -g $1) --rm -v $(pwd):/src metro python -u process_vehicles.py
+docker run --cpus=".75" -u $(id -u $1):$(id -g $1) --rm -v $(pwd):/src metro python -u process_vehicles.py
 if [ $? -eq 0 ]; then
   echo "Successfully processed vehicles:" $(date)
 else
