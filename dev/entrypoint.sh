6469f4f1baf19157eeadbffa84d351a2644014fa
Cameron Sexton <cameron.t.sexton@gmail.com>
2019-08-21 00:05:19 -0700

    remove unnecessary old stuff

--- dev/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ~/venv/bin/activate
-cd ~/vol
-jupyter lab --ip=0.0.0.0 --no-browser
--- dev/setup_venv.sh
+++
@@ -1,5 +0,0 @@
-python3 -m venv venv
-source venv/bin/activate
-pip install jupyterlab
-pip install -r requirements.txt
-deactivate
