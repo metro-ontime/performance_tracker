9f77b84b3ca16dd12061ec1ade8a8ab6ea5d4db3
Maftie <scottmichcohen@yahoo.com>
2019-10-08 19:57:38 -0700

    Tasks amended

--- tasks/EVERY_MINUTE.sh
+++ tasks/EVERY_MINUTE.sh
@@ -1,6 +1,2 @@
 python src/main.py GET_VEHICLES &&
-python src/main.py PREPROCESS_VEHICLES &&
-if [ "$DATASTORE_NAME" == "S3" ]
-then
-    python src/main.py UPLOAD_PREPROCESSED
-fi
+python src/main.py PREPROCESS_VEHICLES
