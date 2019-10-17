211c760ad2563ec841faf2ad5b99a78fe7720a53
maftie <scottmichcohen@yahoo.com>
2019-10-16 19:36:07 -0700

    implement process vehicles

--- src/library/process_vehicles.py
+++ src/library/process_vehicles.py
@@ -11,13 +11,25 @@ def process_vehicles(ctx, datetime):
     agency = ctx.config["METRO_AGENCY"]
     lines = ctx.config["METRO_LINES"]
     date = datetime.in_tz(ctx.config["TIMEZONE"]).format("YYYY-MM-DD")
+    preprocessed_path = ctx.tmp.get_abs_path(f"tracking/{agency}/preprocessed.csv")
+    processed_path = ctx.tmp.get_abs_path(f"tracking/{agency}/processed.csv")
+
+    allData = pd.read_csv(preprocessed_path, index_col=0)
+
+    try:
+        df = pd.read_csv(processed_path, index_col=0)
+    except:
+        df = pd.DataFrame(columns=["datetime", "trip_id", "direction_id", "relative_position"])
+
     for line in lines:
         try:
-            path = ctx.tmp.get_abs_path(f"tracking/{agency}/{line}/preprocessed.csv")
-            df = pd.read_csv(path, index_col=0)
+            #path = ctx.tmp.get_abs_path(f"tracking/{agency}/{line}/preprocessed.csv")
+            lineData = allData[allData['line']==line] 
             tracks = get_track(line, ctx.config["LOCAL_DATA"])
-            processed = process_raw_vehicles(df, tracks)
-            ctx.tmp.write(f"tracking/{agency}/{line}/processed.csv", processed.to_csv())
-        except:
+            processed = process_raw_vehicles(lineData, tracks)
+            df = pd.concat([df, processed], ignore_index=True, sort=False, join="inner")
+        except Exception as e:
+            print(datetime, e)
             continue
+        ctx.tmp.write(f"tracking/{agency}/{line}/processed.csv", processed.to_csv())
     return 0
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3.6
 import sys
 import json
 import pendulum
