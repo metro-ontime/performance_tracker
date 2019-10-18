e339349aaadc7abf216d09bd408fbe625bab782f
maftie <scottmichcohen@yahoo.com>
2019-10-17 17:21:42 -0700

    fixed process_vehicles

--- src/library/process_vehicles.py
+++ src/library/process_vehicles.py
@@ -12,24 +12,16 @@ def process_vehicles(ctx, datetime):
     lines = ctx.config["METRO_LINES"]
     date = datetime.in_tz(ctx.config["TIMEZONE"]).format("YYYY-MM-DD")
     preprocessed_path = ctx.tmp.get_abs_path(f"tracking/{agency}/preprocessed.csv")
-    processed_path = ctx.tmp.get_abs_path(f"tracking/{agency}/processed.csv")
-
     allData = pd.read_csv(preprocessed_path, index_col=0)
-
-    try:
-        df = pd.read_csv(processed_path, index_col=0)
-    except:
-        df = pd.DataFrame(columns=["datetime", "trip_id", "direction_id", "relative_position"])
 
     for line in lines:
         try:
-            #path = ctx.tmp.get_abs_path(f"tracking/{agency}/{line}/preprocessed.csv")
             lineData = allData[allData['line']==line] 
             tracks = get_track(line, ctx.config["LOCAL_DATA"])
             processed = process_raw_vehicles(lineData, tracks)
-            df = pd.concat([df, processed], ignore_index=True, sort=False, join="inner")
+            ctx.tmp.write(f"tracking/{agency}/{line}/processed.csv", processed.to_csv())
         except Exception as e:
             print(datetime, e)
             continue
-        ctx.tmp.write(f"tracking/{agency}/{line}/processed.csv", processed.to_csv())
+
     return 0
