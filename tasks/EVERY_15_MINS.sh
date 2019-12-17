40dd8b37d802f9a13f9f323138593cf0bcbaa7b4
Cam Sexton <cameron.t.sexton@gmail.com>
2019-12-17 01:54:35 -0800

    remove S3 logic from tasks, as it is built into config

--- tasks/EVERY_15_MINS.sh
+++ tasks/EVERY_15_MINS.sh
@@ -1,6 +1,2 @@
 python src/main.py PROCESS_VEHICLES &&
-python src/main.py ESTIMATE_ARRIVALS &&
-if [ "$DATASTORE_NAME" == "S3" ]
-then
-    python src/main.py UPLOAD_VEHICLES_AND_SUMMARY
-fi
+python src/main.py ESTIMATE_ARRIVALS
