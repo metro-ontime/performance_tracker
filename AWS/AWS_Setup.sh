492fafc1497310c5067939a4cbf7ceab975523d9
maftie <scottmichcohen@yahoo.com>
2020-01-03 10:54:23 -0800

    modify setup script

--- AWS/AWS_Setup.sh
+++ AWS/AWS_Setup.sh
@@ -11,6 +11,8 @@ set -e
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 docker pull "$IMAGE"
+usermod -aG docker ubuntu
+newgrp docker
 set +e
 )
 
@@ -31,7 +33,6 @@ AWS_SECRET_ACCESS_KEY=<secret key>
 EOF
 
 #allow for timestamps on STDERR
-
 (
 set -e
 mkdir /home/ubuntu/utils
@@ -42,47 +43,51 @@ while read -r line; do
    echo "[\$(date +%Y-%m-%d\ %H:%M:%S)] \$line" >> /home/ubuntu/logs/errors.log
 done
 EOF
-chmod 755 /home/ubuntu/utils/timestamp_errors.sh
-chown -R ubuntu /home/ubuntu/utils
+chmod -R 777 ubuntu /home/ubuntu/utils
 set +e
 )
 
 #Create Shell Scripts
+(
+set -e
 mkdir /home/ubuntu/logs
 touch /home/ubuntu/logs/errors.log
 touch /home/ubuntu/logs/output.log
-chown -R ubuntu /home/ubuntu/logs 
+chmod -R 777 ubuntu /home/ubuntu/logs 
+set +e
+)
+
+mkdir /home/ubuntu/scripts
 
 (
 set -e
-cat > /home/ubuntu/daily_at_2.sh <<EOF
+cat > /home/ubuntu/scripts/daily_at_2.sh <<EOF
 #!/bin/bash
-sudo docker run --rm --env-file /home/ubuntu/docker_config/.env -v /home/ubuntu/data/tmp:/app/data/tmp "$IMAGE" bash ./tasks/DAILY_AT_2AM.sh
+docker run --rm --env-file /home/ubuntu/docker_config/.env -v /home/ubuntu/data/tmp:/app/data/tmp "$IMAGE" bash ./tasks/DAILY_AT_2AM.sh
 EOF
-chmod 755 daily_at_2.sh
 set +e
 )
 
 (
 set -e
-cat > /home/ubuntu/every_minute.sh <<EOF
+cat > /home/ubuntu/scripts/every_minute.sh <<EOF
 #!/bin/bash
-sudo docker run --rm --env-file /home/ubuntu/docker_config/.env -v /home/ubuntu/data/tmp:/app/data/tmp "$IMAGE" bash ./tasks/EVERY_MINUTE.sh
+docker run --rm --env-file /home/ubuntu/docker_config/.env -v /home/ubuntu/data/tmp:/app/data/tmp "$IMAGE" bash ./tasks/EVERY_MINUTE.sh
 EOF
-chmod 755 every_minute.sh
 set +e
 )
 
 #run the EVERY_15_MIN tasks every hour in dev to reduce the amount of writes to S3 (free tier caps at 2k per month) 
 (
 set -e
-cat > /home/ubuntu/every_hour.sh <<EOF
+cat > /home/ubuntu/scripts/every_hour.sh <<EOF
 #!/bin/bash
-sudo docker run --rm --env-file /home/ubuntu/docker_config/.env -v /home/ubuntu/data/tmp:/app/data/tmp "$IMAGE" bash ./tasks/EVERY_15_MINS.sh
+docker run --rm --env-file /home/ubuntu/docker_config/.env -v /home/ubuntu/data/tmp:/app/data/tmp "$IMAGE" bash ./tasks/EVERY_15_MINS.sh
 EOF
-chmod 755 every_hour.sh
 set +e
 )
+
+chmod -R 777 /home/ubuntu/scripts
 
 #Configure cron tasks
 
@@ -90,9 +95,9 @@ set +e
 set -e
 cat > every_minute <<EOF
 CRON_TZ=UTC
-* * * * * root /home/ubuntu/every_minute.sh 2> >(/home/ubuntu/utils/timestamp_errors.sh) >> /home/ubuntu/logs/output.log
+SHELL=/bin/bash
+* * * * * root /home/ubuntu/scripts/every_minute.sh 2> >(/home/ubuntu/utils/timestamp_errors.sh) >> /home/ubuntu/logs/output.log
 EOF
-chmod 644 every_minute
 mv every_minute /etc/cron.d
 set +e
 )
@@ -101,9 +106,9 @@ set +e
 set -e
 cat > every_hour <<EOF
 CRON_TZ=UTC
+SHELL=/bin/bash
 0 * * * * root /home/ubuntu/every_hour.sh 2> >(/home/ubuntu/utils/timestamp_errors.sh) >> /home/ubuntu/logs/output.log 
 EOF
-chmod 644 every_hour
 mv every_hour /etc/cron.d
 set +e
 )
@@ -112,12 +117,13 @@ set +e
 set -e
 cat > every_day <<EOF
 CRON_TZ=UTC
-* 11 * * * root rm -rf ~/data/tmp && /home/ubuntu/daily_at_2.sh 2> >(/home/ubuntu/utils/timestamp_errors.sh) >> /home/ubuntu/logs/output.log
+SHELL=/bin/bash
+* 10 * * * root rm -rf ~/data/tmp && /home/ubuntu/daily_at_2.sh 2> >(/home/ubuntu/utils/timestamp_errors.sh) >> /home/ubuntu/logs/output.log
 EOF
-chmod 644 every_day
 mv every_day /etc/cron.d
 set +e
 )
+chmod -R 755 /etc/cron.d
 
 #run the daily script once on launch to obtain schedule for current day
-bash /home/ubuntu/daily_at_2.sh 2> >(/home/ubuntu/utils/timestamp_errors.sh) >> /home/ubuntu/logs/output.log
+bash /home/ubuntu/scripts/daily_at_2.sh 2> >(/home/ubuntu/utils/timestamp_errors.sh) >> /home/ubuntu/logs/output.log
